bdfc19992fffcfcbde100b7483bdbc427e59c636
S_HE <stanhe@gmail.com>
2019-04-30 13:32:58 -0700

    Import project

---
+++ GUICalendar/src/CalendarTester.java
@@ -0,0 +1,14 @@
+import java.util.Calendar;
+
+import javax.swing.JFrame;
+
+public class CalendarTester {
+
+	public static void main(String[] args)
+	{
+		JFrame frame = new CalendarFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+}
